3ba6d60e6b6f0953e2700994e357b2cdb5230416
Jimike <oladoyemike@gmail.com>
2024-03-03 00:14:47 +0300

    Developed the Navbar and Hero Banner sections

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     <Stack
       direction="row"
       justifyContent="space-around"
-      sx={{ gap: { sm: "122px", xs: "480px" }, mt: { sm: "32px", xs: "20px" } }}
+      sx={{ gap: { sm: "122px", xs: "40px" }, mt: { sm: "32px", xs: "20px" }, justifyContent: "none" }} px="20px"
     >
       <Link to="/">
         <img
@@ -26,6 +26,7 @@ const Navbar = () => {
             borderBottom: "3px solid #FF3625",
           }}
         >Home</Link>
+        <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
       </Stack>
     </Stack>
   );
