1777d1ec80aa8add07e9c1dbe096fd3abb950e7f
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2024-03-07 09:38:54 +0000

    This has bugs. Will fix soon

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [exerciseNames, setExerciseNames] = useState([]);
+
+  useEffect(() => {
+    const fetchExerciseNames = async () => {
+      const data = await fetchData("exercises", exerciseOptions);
+      setExerciseNames(data);
+    };
+
+    fetchExerciseNames();
+  }, []);
+
+  console.log(exerciseNames);
+
   return (
-    <div>
-      Exercises
-    </div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exerciseNames.map((exercise, index) => (
+          <p>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 
-export default Exercises
+export default Exercises;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
-import HeroBanner from "../components/HeroBanner";
-import SearchExercises from "../components/SearchExercises";
+
 import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
@@ -11,8 +12,16 @@ const Home = () => {
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={setExercises} bodyPart={bodyPart} exercises={exercises} />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
