e8c332261fea9ea6b0cec09f8a23e67ac86261d9
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2024-03-11 14:02:14 +0000

    Implemented the Exercise Details page

---
+++ src/components/Detail.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Typography, Stack, Button } from "@mui/material";
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, instructions, gifUrl, target, name, equipment } =
+    exerciseDetail;
+
+  const extraDetail = [
+    { name: bodyPart, icon: BodyPartImage },
+    { name: target, icon: TargetImage },
+    { name: equipment, icon: EquipmentImage },
+  ];
+
+  return (
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography textTransform="capitalize" variant="h3">{name}</Typography>
+        <Typography variant="h6">{instructions}</Typography>
+        {extraDetail.map((detail, index) => (
+          <Stack
+            key={index}
+            direction="row"
+            gap="24px"
+            alignItems="center"
+            sx={{ fontSize: "18px" }}
+          >
+            <Button sx={{ background: "#FFF2DB", borderRadius: "50%", width: "100px", height: "100px" }}>
+            <img src={detail.icon} alt={bodyPart} style={{ width: "50px", height: "50px" }} />
+            </Button>
+            <Typography textTransform="capitalize" variant="h5">
+              {detail.name}
+              </Typography>
+          </Stack>
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Detail;
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseVideos from '../components/ExerciseVideos';
+import Detail from '../components/Detail';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExerciseDetail = async () => {
+      const exerciseDBUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com";
+      const exerciseDetailData = await fetchData(`${exerciseDBUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+    }
+    fetchExerciseDetail();
+  }, [id]);
+  
   return (
-    <div>
-      Exercise Detail
-    </div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
